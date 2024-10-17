d167998c09d384aa200e28d04d45d3c0c31b11ee
Sam Robinson <robinson.will.sam@gmail.com>
2024-10-17 12:55:22 -0500

    udemy 58

--- index.js
+++ index.js
@@ -159,3 +159,64 @@
 // printAllNumbersThenAllPairSums([1,2,3,4,5]);
 
 // O(n^2)
+
+//Given two arrays, create a function that lets a user know
+//true/false
+// whether these two arrays contain any common items
+//EX. 
+//const array1 = ['a', 'b', 'c', 'x']
+//const array2 = ['z', 'y', 'x']
+//should return true
+
+// const array1 = ['a', 'b', 'c', 'x']
+// const array2 = ['z', 'y', 'x']
+
+// function sameBruteForce(arr1, arr2){
+//   for(let i = 0; i < arr1.length; i++){
+//     for(let j = 0; j < arr2.length; j++){
+//       if (arr1[i] === arr2[j]){
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+
+// sameBruteForce(array1, array2)
+
+
+//use hash table 
+
+const array1 = ['a', 'b', 'c', 'x']
+const array2 = ['z', 'y', 'x']
+
+//array1 ==> object {
+//a: true
+//b: true
+//c: true
+//x: true
+//}
+//array2[index] === object.properties
+
+function sameHash(arr1, arr2){
+  //loop through first array and create object with properties = array items
+  let map = {};
+  for(let i = 0; i < arr1.length; i++){
+    if(!map[arr1[i]]){
+      const item = arr1[i];
+      map[item] = true;
+    }
+  }
+  //loop through second array and check if item exists in object
+  for (let j = 0; j < arr2.length; j++){
+    if(map[arr2[j]]){
+      return true
+    }
+  }
+  return false;
+}
+//0(a+b)
+sameHash(array1, array2);
+
+
