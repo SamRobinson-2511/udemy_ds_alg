afaacb4f8959c9d114e5dd3ccf2a05e9e773312f
Sam Robinson <robinson.will.sam@gmail.com>
2024-10-17 16:26:27 -0500

    objects in JS

--- index.js
+++ index.js
@@ -188,8 +188,8 @@
 
 //use hash table 
 
-const array1 = ['a', 'b', 'c', 'x']
-const array2 = ['z', 'y', 'x']
+// const array1 = ['a', 'b', 'c', 'x']
+// const array2 = ['z', 'y', 'x']
 
 //array1 ==> object {
 //a: true
@@ -199,24 +199,86 @@ const array2 = ['z', 'y', 'x']
 //}
 //array2[index] === object.properties
 
-function sameHash(arr1, arr2){
-  //loop through first array and create object with properties = array items
-  let map = {};
-  for(let i = 0; i < arr1.length; i++){
-    if(!map[arr1[i]]){
-      const item = arr1[i];
-      map[item] = true;
-    }
-  }
-  //loop through second array and check if item exists in object
-  for (let j = 0; j < arr2.length; j++){
-    if(map[arr2[j]]){
-      return true
-    }
-  }
-  return false;
-}
-//0(a+b)
-sameHash(array1, array2);
+// function sameHash(arr1, arr2){
+//   //loop through first array and create object with properties = array items
+//   let map = {};
+//   for(let i = 0; i < arr1.length; i++){
+//     if(!map[arr1[i]]){
+//       const item = arr1[i];
+//       map[item] = true;
+//     }
+//   }
+//   //loop through second array and check if item exists in object
+//   for (let j = 0; j < arr2.length; j++){
+//     if(map[arr2[j]]){
+//       return true
+//     }
+//   }
+//   return false;
+// }
+// //0(a+b)
+// sameHash(array1, array2);
 
 
+//cleaner language with methods
+// const array1 = ['a', 'b', 'c', 'x']
+// const array2 = ['z', 'y', 'x']
+
+// function cleanHash(arr1, arr2){
+//   return arr1.some(item => arr2.includes(item));
+// }
+
+// cleanHash(array1, array2);
+
+//Two Sum
+// function hasPairWithSums(arr, sum){
+//   const len = arr.length;
+//   for(let i =0; i < len; i++){
+//     for(let j = 0; j < len; j++){
+//       if (arr[i] + arr[j] === sum){
+//         return true;
+//       }
+//     }
+//   }
+//   return false;
+// }
+
+
+
+
+//Better
+// function hasPairWithSums2(arr, sum){
+//   const mySet = new Set();
+//   const len = arr.length;
+//   for (let i = 0; i < len; i++){
+//     if (mySet.has(arr[i])){
+//       return true;
+//     }
+//     mySet.add(sum - arr[i]);
+//   }
+//   return false;
+// }
+
+// hasPairWithSums2([6, 4, 3, 2, 1, 7], 9)
+
+const strings = ['a', 'b', 'c', 'd']
+//4*4 = 16 bytes of storage
+
+//access
+strings[2]
+
+
+//push
+strings.push('e'); //O(1)
+
+//pop
+strings.pop(); //O(1)
+
+
+//unshift
+strings.unshift('x'); //O()
+
+
+//splice
+strings.splice(2, 0, 'alien'); //O(n)
+// console.log(strings);
---
+++ objects.js
@@ -0,0 +1,61 @@
+// const object1 = { value: 10 };
+// const object2 = object1;
+// const object3 = { value: 10 };
+
+//Reference type:
+// object
+
+
+//Context: 
+//tells you where we are within the object
+//this.
+// console.log(this)
+
+
+//instantiation
+// class Player {
+//   constructor(name, type){
+//     console.log('player', this);
+//     this.name = name;
+//     this.type = type;
+//   }
+//   introduce() {
+//     console.log(`Hi I am ${this.name}, I'm a ${this.type}`)
+//   }
+// }
+
+// class Wizard extends Player {
+//   constructor(name, type) {
+//     super(name, type)
+//   }
+//   play() {
+//     console.log(`Weeee I'm a ${this.type}`)
+//   }
+// }
+
+// const wizard1 = new Wizard("Shelly", "Healer")
+// const wizard2 = new Wizard("Shawn", "Dark Magic")
+
+
+//classical inheritance - deprecated
+const Player = function(name, type){
+  this.name = name;
+  this.type = type;
+}
+
+Player.prototype.introduce = function(){
+  console.log(`Hi I am ${this.name}, I'm a ${this.type}`);
+}
+
+const wizard1 = new Player("Shelly", "Healer");
+const wizard2 = new Player("Shawn", "dark magic");
+
+wizard1.play = function(){
+  console.log(`Weeeee I'm a ${this.type}`);
+}
+
+wizard2.play = function(){
+  console.log(`Weeeee I'm a ${this.type}`);
+}
+
+
