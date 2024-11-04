ab986bdaedfe78892ecaa890ec0115d25255ab4f
Sam Robinson <robinson.will.sam@gmail.com>
2024-11-04 06:28:45 -0600

    hash tables

--- hash_table.js
+++ hash_table.js
@@ -1,8 +1,63 @@
-let user = {
-  age: 54, 
-  name: 'Kylie', 
-  magic: true, 
-  scream: function () {
-    console.log('Ahhhhhh');
+// let user = {
+//   age: 54, 
+//   name: 'Kylie', 
+//   magic: true, 
+//   scream: function () {
+//     console.log('Ahhhhhh');
+//   }
+// }
+
+// console.log(user.magic);
+
+
+// const a = new Map(); // can save any data type as a key, maintains insertion order
+// const b = new Sets(); //stores keys, no values
+
+
+class HashTable{
+  constructor(size){
+    this.data = new Array(size);
+  }
+
+  _hash(key){
+    let hash = 0; 
+    for (let i = 0; i < key.length; i++){
+      hash = (hash + key.charCodeAt(i)*i) %
+      this.data.length;
+      // console.log(hash);
+    }
+    return hash;
+  }
+
+  set(key, value){
+    let address = this._hash(key);
+    if(!this.data[address]){
+      this.data[address] = [];
+      console.log(this.data);
+    } 
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+
+  get(key){
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+    // console.log(currentBucket);
+    if (currentBucket){
+      for(let i = 0; i < currentBucket.lenght; i++){
+        if(currentBucket[i][0] === key){
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
   }
 }
+
+const myHashTable = new HashTable(50);
+myHashTable.set('apples', 54); //bucket
+myHashTable.set('grapes', 10000);
+myHashTable.get('apples');
+// myHashTable._hash('grapes'); //private property 
+
+// console.log('test');
