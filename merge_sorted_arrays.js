65efd83f65d1ecd37e91c679f4f0aedf0627d2e0
Sam Robinson <robinson.will.sam@gmail.com>
2024-11-05 01:38:44 -0600

    started doubly linked lists but want to migrate a bunch of this stuff to one source

--- merge_sorted_arrays.js
+++ merge_sorted_arrays.js
@@ -1,36 +1,36 @@
-function mergeSortedArrays(arr1, arr2){
-  const mergedArray = [];
-  let array1Item = arr1[0];
-  let array2Item = arr2[0];
-  let i = 1;
-  let j = 1;
+// function mergeSortedArrays(arr1, arr2){
+//   const mergedArray = [];
+//   let array1Item = arr1[0];
+//   let array2Item = arr2[0];
+//   let i = 1;
+//   let j = 1;
 
-  //check input
-  if (arr1.length === 0){
-    return arr2;
-  }
-  if (arr2.length === 0){
-    return arr1;
-  }
+//   //check input
+//   if (arr1.length === 0){
+//     return arr2;
+//   }
+//   if (arr2.length === 0){
+//     return arr1;
+//   }
 
-  while(array1Item || array2Item){
-    console.log(array1Item, array2Item)
-    if (!array2Item < array2Item) {
-      mergedArray.push(array1Item);
-      array1Item = arr1[i];
-      i++;
-    } else {
-      mergedArray.push(array2Item);
-      array2Item = arr2[j];
-    }
+//   while(array1Item || array2Item){
+//     console.log(array1Item, array2Item)
+//     if (!array2Item < array2Item) {
+//       mergedArray.push(array1Item);
+//       array1Item = arr1[i];
+//       i++;
+//     } else {
+//       mergedArray.push(array2Item);
+//       array2Item = arr2[j];
+//     }
 
 
-  }
+//   }
   
   
   
-  return mergedArray;
-}
+//   return mergedArray;
+// }
 
 
-mergeSortedArrays([0, 2, 4, 6], [1, 3, 5, 7]);
+// mergeSortedArrays([0, 2, 4, 6], [1, 3, 5, 7]);
