17386651dfe460535597c29176820a0b9f259dd4
Sam Robinson <robinson.will.sam@gmail.com>
2024-11-04 22:56:46 -0600

    linked lists

--- linked_list.js
+++ linked_list.js
@@ -1,4 +1,4 @@
-const basket = ['apples', 'grapes', 'pears'];
+
 
 //pointers
 // const obj1 = {a: true};
@@ -9,11 +9,60 @@ const basket = ['apples', 'grapes', 'pears'];
 
 
 //garbage collection 
-let obj1 = {a: true};
-let obj2 = obj1;
-obj1.a = 'booya';
-delete obj1;
-// console.log('1', obj1);
-console.log('2', obj2);
+// let obj1 = {a: true};
+// let obj2 = obj1;
+// obj1.a = 'booya';
+// delete obj1;
+// // console.log('1', obj1);
+// console.log('2', obj2);
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null
+//       }
+//     }
+//   }
+// }
+
+class LinkedList {
+  constructor(value){
+    this.head = {
+      value: value, 
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value){
+    const newNode = {
+      value: value, 
+      next: null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+  prepend(value){
+    const newNode = {
+      value: value, 
+      next: null
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+console.log(myLinkedList.append(5));
+console.log(myLinkedList.append(16));
+console.log(myLinkedList.prepend(1));
 
 
